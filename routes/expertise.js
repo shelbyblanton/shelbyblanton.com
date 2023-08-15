b812f7b90cbd32b93d9916b6f050bbe4f9f6de53
M. Shelby Blanton <shelby@Ms-MacBook-Pro.local>
2023-08-15 06:45:42 -0700

    Remove Separate Routes, Add CSS and Images

--- routes/expertise.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('expertise', { title: 'Michael Shelby Blanton - Phoenix, AZ - Expertise' });
-});
-
-module.exports = router;
