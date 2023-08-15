075bc88ae98e748d1802d8a40af53ee14182ea26
M. Shelby Blanton <shelby@Ms-MacBook-Pro.local>
2023-08-15 08:05:55 -0700

    Add Menu Views & Routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+
+router.get('/background', function(req, res, next) {
+  res.render('background', { title: 'Michael Shelby Blanton - Phoenix, AZ - Background' });
+});
+
+router.get('/certifications', function(req, res, next) {
+  res.render('certifications', { title: 'Michael Shelby Blanton - Phoenix, AZ - Certifications' });
+});
+
+router.get('/education', function(req, res, next) {
+  res.render('education', { title: 'Michael Shelby Blanton - Phoenix, AZ - Education' });
+});
+
+router.get('/experience', function(req, res, next) {
+  res.render('experience', { title: 'Michael Shelby Blanton - Phoenix, AZ - Experience' });
+});
+
+router.get('/expertise', function(req, res, next) {
+  res.render('expertise', { title: 'Michael Shelby Blanton - Phoenix, AZ - Expertise' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Michael Shelby Blanton - Phoenix, AZ - Projects' });
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Michael Shelby Blanton - Phoenix, AZ' });
